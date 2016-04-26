47a8faf8188163f01cbe662924aa4ffd51970cbe
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-04-26 15:59:01 +0200

    Iteration 3

--- app/src/components/CharacterCard/CharacterCard.jsx
+++ app/src/components/CharacterCard/CharacterCard.jsx
@@ -3,10 +3,11 @@ import {Component} from 'react';
 
 export default class CharacterCard extends Component {
   renderCharacterSeries() {
-    if (this.props.character.series.items.length) {
-      return <span>{this.props.character.series.items[0].name}</span>;
-    }
     return null;
+  }
+
+  getCharacterImageSrc(path) {
+    return `${path}/standard_medium.jpg`;
   }
 
   render() {
@@ -14,18 +15,18 @@ export default class CharacterCard extends Component {
       <div className="col-xs-4 characterListItem">
         <div className="characterCard card row">
           <div className="col-xs-4">
-            <img className="characterCardImage" src={this.props.character.thumbnail.path + '/standard_medium.jpg'}/>
+            <img className="characterCardImage" src={this.getCharacterImageSrc('example/image/path/')}/>
           </div>
           <div className="col-xs-8">
             <div className="characterCardHeader">
-              <h5 className="characterCardName">{this.props.character.name}</h5>
+              <h5 className="characterCardName">{'Example character name'}</h5>
             </div>
             <div className="characterCardSeries">
               {this.renderCharacterSeries()}
             </div>
           </div>
 
-          <a className="characterCardLink" href={this.props.character.urls[0].url}><i
+          <a className="characterCardLink" href={'http://example-link'}><i
             className="material-icons">link</i></a>
         </div>
       </div>
