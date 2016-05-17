807effb984a754c10a27086bfbc56ca20a7021a3
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-05-17 14:32:57 +0200

    Iteration 4

--- app/src/reducers/characterList.js
+++ app/src/reducers/characterList.js
@@ -1,20 +1,33 @@
 import {CHARACTERS_LIST_REQUEST, CHARACTERS_LIST_SUCCESS, CHARACTERS_LIST_SEARCH} from '../actions';
 
 const initialState = {
+  items: [],
   isLoading: false,
-  filteredItems: [],
-  allItems: [],
-  query: ''
+  searchText: ''
 };
 
 export default function charactersList(state = initialState, action = null) {
   switch (action.type) {
     case CHARACTERS_LIST_REQUEST:
-      return state;
+      return Object.assign({}, state, {
+        isLoading: true
+      });
     case CHARACTERS_LIST_SUCCESS:
-      return state;
+      const items = action.payload;
+      return Object.assign({}, state, {
+        allItems: items,
+        items: items,
+        isLoading: false
+      });
     case CHARACTERS_LIST_SEARCH:
-      return state;
+      const {searchText} = action;
+      const filteredItems = state.allItems.filter((item) => {
+        const name = item.name.toLowerCase();
+        return !action.searchText.length || name.indexOf(searchText.toLocaleLowerCase()) !== -1;
+      });
+      return Object.assign({}, state, {
+        searchText, items: filteredItems
+      });
     default:
       return state;
   }
